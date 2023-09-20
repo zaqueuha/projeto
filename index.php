7a780a5c46a47a3187500ba021b8a9e2a371ca3f
Sergio Lavor <127288387+prof-sergio-lavor@users.noreply.github.com>
2023-09-20 14:41:02 -0300

    v1-modelo

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>listagem dos jogos</title>
+</head>
+<body>
+    <div id="corpo">
+    <h1>Escolha Um Jogo</h1>
+    <table class="listagem">
+        <tr><td>Nome <td>Foto <td>Adm
+        <tr><td>Nome <td>Foto <td>Adm
+        <tr><td>Nome <td>Foto <td>Adm
+        <tr><td>Nome <td>Foto <td>Adm
+    </table>
+    </div>
+    
+</body>
+</html>
---
+++ modelo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto</title>
+</head>
+<body>
+    <div id="corpo">
+    <h1>olá</h1>
+    </div>
+</body>
+</html>
